94e75677d6dcdd4f5ce2c65a4318465d0df681b9
RolandBende <31572281+RolandBende@users.noreply.github.com>
2017-09-03 22:25:52 +0200

    v1.0

---
+++ clean_wp_head.php
@@ -0,0 +1,106 @@
+<?php
+/*
+Plugin Name: Clean WP. HEAD
+Plugin URI: bwebdesign.hu
+Description: Removes unnecessary lines / tags from Wordpress HEAD section.
+Version: 1.0.0
+Author: Roland Bende
+Author URI: bwebdesign.hu
+*/
+
+// Remove WP Generator, RSD, XMLRPC, WLW, ShortLink and Comment Feed links, Remove prev and next article links
+remove_action('wp_head', 'wp_generator');
+remove_action('wp_head', 'rsd_link');
+remove_action('wp_head', 'feed_links', 2);
+remove_action('wp_head', 'index_rel_link');
+remove_action('wp_head', 'wlwmanifest_link');
+remove_action('wp_head', 'feed_links_extra', 3);
+remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+remove_action('wp_head', 'start_post_rel_link', 10, 0);
+remove_action('wp_head', 'parent_post_rel_link', 10, 0);
+remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);
+
+
+// Remove WP Emoji
+
+	/**
+	 * Disable the emoji's
+	 */
+	function disable_emojis() {
+		remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+		remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+		remove_action( 'wp_print_styles', 'print_emoji_styles' );
+		remove_action( 'admin_print_styles', 'print_emoji_styles' );	
+		remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+		remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );	
+		remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+		add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+		add_filter( 'wp_resource_hints', 'disable_emojis_remove_dns_prefetch', 10, 2 );
+	}
+	add_action( 'init', 'disable_emojis' );
+
+	/**
+	 * Filter function used to remove the tinymce emoji plugin.
+	 * 
+	 * @param    array  $plugins  
+	 * @return   array             Difference betwen the two arrays
+	 */
+	function disable_emojis_tinymce( $plugins ) {
+		if ( is_array( $plugins ) ) {
+			return array_diff( $plugins, array( 'wpemoji' ) );
+		} else {
+			return array();
+		}
+	}
+
+	/**
+	 * Remove emoji CDN hostname from DNS prefetching hints.
+	 *
+	 * @param  array  $urls          URLs to print for resource hints.
+	 * @param  string $relation_type The relation type the URLs are printed for.
+	 * @return array                 Difference betwen the two arrays.
+	 */
+	function disable_emojis_remove_dns_prefetch( $urls, $relation_type ) {
+
+		if ( 'dns-prefetch' == $relation_type ) {
+
+			// Strip out any URLs referencing the WordPress.org emoji location
+			$emoji_svg_url_bit = 'https://s.w.org/images/core/emoji/';
+			foreach ( $urls as $key => $url ) {
+				if ( strpos( $url, $emoji_svg_url_bit ) !== false ) {
+					unset( $urls[$key] );
+				}
+			}
+
+		}
+
+		return $urls;
+	}
+
+	// Remove REST API functions
+function remove_json_api () {
+
+    // Remove the REST API lines from the HTML Header
+    remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
+    remove_action( 'wp_head', 'wp_oembed_add_discovery_links', 10 );
+
+    // Remove the REST API endpoint.
+    remove_action( 'rest_api_init', 'wp_oembed_register_route' );
+
+    // Turn off oEmbed auto discovery.
+    add_filter( 'embed_oembed_discover', '__return_false' );
+
+    // Don't filter oEmbed results.
+    remove_filter( 'oembed_dataparse', 'wp_filter_oembed_result', 10 );
+
+    // Remove oEmbed discovery links.
+    remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
+
+    // Remove oEmbed-specific JavaScript from the front-end and back-end.
+    remove_action( 'wp_head', 'wp_oembed_add_host_js' );
+
+   // Remove all embeds rewrite rules.
+   //add_filter( 'rewrite_rules_array', 'disable_embeds_rewrites' );
+
+}
+add_action( 'after_setup_theme', 'remove_json_api' );
